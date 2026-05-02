e53ac173e35e0340991cdf646d86020bf33e54a3
Harshal Khadatare <khadtareharshal2002@gmail.com>
2026-05-02 17:33:41 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int[] numbers = { 1, 2, 2, 3, 4, 4, 5, 6, 6 };
+
+        // LINQ query syntax to get unique values
+        var uniqueNumbers = (from n in numbers
+                             select n).Distinct();
+
+        Console.WriteLine("Unique Numbers:");
+
+        foreach (var num in uniqueNumbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
