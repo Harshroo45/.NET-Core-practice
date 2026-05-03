52ea38a8fbd26e2775ef7ed5f8cd91cdbecbfffe
Harshal Khadatare <khadtareharshal2002@gmail.com>
2026-05-03 16:24:45 +0530

    new linq program 3

---
+++ ConsoleApp1/AggregateBy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class AggregateBy
+    {
+        (string name, string city, int age)[] arr = { 
+            ("harshal","Mahud",23),
+            ("sayali","sangavi",23),
+            ("rohini","nagpur",23),
+            ("tejas","barshi",23)
+        };
+
+
+    }
+}
